064600bdae5dff8d36d4317f1ebae17e1e7a931b
paltoalko <martas90909@me.com>
2023-03-17 13:33:57 +0100

    changed names + types

--- src/helpers/typings.ts
+++ src/helpers/typings.ts
@@ -1,12 +1,12 @@
 export type FormType = {
 	name: string;
-	time: string;
+	preparation_time: string;
 	type: string;
 	no_of_slices?: number;
 	diameter?: number;
 	spiciness_scale?: number;
 	slices_of_bread?: number;
-	id: string;
+	id: number;
 };
 
 export type FormValues = {
